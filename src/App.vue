48e6941bcf114c611aab55549830023b7d2b6351
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-01-31 22:37:14 -0600

    get de characters

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <h1>Rick And Morty</h1>
+  <ListCharacters />
 </template>
 
 <script>
-
+import ListCharacters from './components/ListCharacters.vue';
 export default {
   name: 'App',
   components: {
+    ListCharacters
   }
 }
 </script>
---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <div class="characters">
+            <div class="characters__item" v-for="characters in character" :key="character.id">
+                <h2>{{ character.name }}</h2>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+export default{
+    setup(){
+        const store = useStore();
+        const characters = computed(()=>{
+            debugger;
+            return store.state.charactersFilter;
+        });
+        onMounted(()=>{
+            store.dispatch('getCharacters')
+        })
+        return {
+            characters
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
